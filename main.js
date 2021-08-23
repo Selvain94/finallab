04718fda20c1ea2efc3001c831ac1894cafe64a4
Selvainthiren A/L Karunanithee <selvain_pg@yahoo.com>
2021-08-23 22:08:54 +0800

    Update to Js
    Print to console (console.log)

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 }
+
+let arrOfStrings =["car", "van", "SUV", "truck", "bikes"];
+
+console.log(arrOfStrings.length);
+
+
+let obj = {
+  name: "Ace Dealership",
+  location: "Kuala Lumpur",
+  service: "Buy Sell Rent",
+};
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+function locationWithArgs(first, last){
+  console.log(first + " " + last);
+}
+locationWithArgs("Kuala", "Lumpur");
+
+let services= {
+    buy: "buy",
+    sell: "sell",
+    rent: "rent",
+};
+alert("buy, sell, rent");
