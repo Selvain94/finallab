c6027573cbbd3377892252490d90c61d0a124dda
Selvainthiren A/L Karunanithee <selvain_pg@yahoo.com>
2021-08-23 23:14:49 +0800

    Added finance.html
    Added loan calculator for users

--- main.js
+++ main.js
@@ -27,3 +27,19 @@ let services= {
     rent: "rent",
 };
 alert("buy, sell, rent");
+
+function Calculate() {
+
+  const amount = document.querySelector("#amount").value;
+
+  const rate = document.querySelector("#rate").value;
+
+  const months = document.querySelector("#months").value;
+
+  const interest = (amount * (rate * 0.01)) / months;
+    
+  const total = ((amount / months) + interest).toFixed(2);
+
+  document.querySelector("#total")
+      .innerHTML = "Instalment : (RM)" + total;
+}  
